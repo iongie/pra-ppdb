e3e9b41475b9d751fbd381af49b1ee1b06d2dcbb
gigih santoso <gigih985042@gmail.com>
2024-05-17 17:28:02 +0700

    modified:   src/app/pages/video/video.component.html 	modified:   src/app/pages/video/video.component.ts 	new file:   src/assets/tutor_daftar.png 	new file:   src/assets/tutor_input.png 	new file:   src/assets/tutor_verif.png

--- src/app/pages/video/video.component.ts
+++ src/app/pages/video/video.component.ts
@@ -7,13 +7,30 @@ import { SafeUrl, DomSanitizer } from '@angular/platform-browser';
   styleUrl: './video.component.css'
 })
 export class VideoComponent implements OnInit {
-  urlAlur : string =  'https://www.youtube.com/embed/voXlG_U3sVc'
-  safeDocsAlur: SafeUrl | null = null;
+  verif : string =  'https://drive.google.com/file/d/1dkZTcsroyqtF3Ywn_ZYtm3DktF1H0Swa/preview'
+  input : string =  'https://drive.google.com/file/d/1ZWo13_iTNWZUDxc9IRe4cRLDCXuJysLk/preview'
+  daftar : string =  'https://drive.google.com/file/d/1RwDna-nfhgj0PjqOrO2rFjLXX8LdtTh3/preview';
+  playVideo: string = 'https://drive.google.com/file/d/1dkZTcsroyqtF3Ywn_ZYtm3DktF1H0Swa/preview';
+  safeVerif: SafeUrl | null = null;
+  safeInput: SafeUrl | null = null;
+  safeDaftar: SafeUrl | null = null;
+  safePlayVideo: SafeUrl | null = null;
+  titlePlayVideo: string = 'Tutorial Input Data Siswa (Operator SD)'
   constructor(
     private sanitizer: DomSanitizer
   ){}
 
   ngOnInit(): void {
-    this.safeDocsAlur = this.sanitizer.bypassSecurityTrustResourceUrl(this.urlAlur)
+    this.safeVerif = this.sanitizer.bypassSecurityTrustResourceUrl(this.verif);
+    this.safeInput = this.sanitizer.bypassSecurityTrustResourceUrl(this.input);
+    this.safeDaftar = this.sanitizer.bypassSecurityTrustResourceUrl(this.daftar);
+    this.safePlayVideo = this.sanitizer.bypassSecurityTrustResourceUrl(this.playVideo);
   }
+
+  gantiPlay(changeSafeUrl: SafeUrl, titlePlayVideo: string){
+    this.safePlayVideo = changeSafeUrl;
+    this.titlePlayVideo = titlePlayVideo 
+  }
+
+
 }
