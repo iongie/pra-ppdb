01e41e47b4f32ade7bbbd4cef6f16c248f7f54e3
gigih santoso <gigih985042@gmail.com>
2024-05-27 09:04:36 +0700

    modified:   src/app/components/footer-utama/footer-utama.component.html 	modified:   src/app/services/call-api/call-api.service.ts

--- src/app/services/call-api/call-api.service.ts
+++ src/app/services/call-api/call-api.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { catchError, throwError } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
